309b8f4de4ff26f5e9b671b7812f5a394f4ed4d7
Nazareno Pesado <nano@bitopia.digital>
2022-04-27 15:42:41 +0000

    feat: deploy the contract to Rinkeby network

--- deploy.js
+++ deploy.js
@@ -1,1 +1,32 @@
-// deploy code will go here
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProvider(
+    'clock still cushion security wire water dance slab valid ship hammer chalk',
+    'https://rinkeby.infura.io/v3/84aafbf69a0c444790c3fdd16c8455cf'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({
+            data: bytecode,
+            arguments: [
+                'Hi there!'
+            ]
+        })
+        .send({
+            from: accounts[0],
+            gas: '1000000',
+        });
+
+    console.log('Contract deployed to', result.options.address)
+    provider.engine.stop();
+};
+deploy();
